875a70fca8b1d5326d5b14d65954f0c6d0b527e6
Doerthous <827484343@qq.com>
2020-03-09 00:57:49 +0800

    update license

--- serial.h
+++ serial.h
@@ -1,12 +1,35 @@
-﻿/**
-  ******************************************************************************
-  * \brief
-  * \file       serial.h
-  * \author     doerthous
-  * \date       2019-09-12
-  * \details
-  ******************************************************************************
-  */
+﻿/*******************************************************************************
+
+  Copyright © 2019
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy 
+  of this software and associated documentation files (the “Software”), to deal 
+  in the Software without restriction, including without limitation the rights 
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+  copies of the Software, and to permit persons to whom the Software is 
+  furnished to do so, subject to the following conditions:
+
+  1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+  The above copyright notice and this permission notice shall be included in 
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+  SOFTWARE.
+
+  Author: doerthous <doerthous@gmail.com>
+
+*******************************************************************************/
+
 
 #ifndef SERIAL_H_
 #define SERIAL_H_
@@ -61,5 +84,3 @@ enum
 void serial_flush(serial_t serial, int option);
 
 #endif /* SERIAL_H_ */
-
-/****************************** Copy right 2019 *******************************/
